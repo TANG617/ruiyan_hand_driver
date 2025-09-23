2c754692284862b038ee9db9bdf8ea86568b7f54
LiTang <litang0617@outlook.com>
2025-09-23 07:56:56 +0000

    feat: milestone

--- test/node/test_node.py
+++ test/node/test_node.py
@@ -21,7 +21,7 @@ import time
 from ruiyan_hand_driver.node import RuiyanHandNode
 
 
-interface = SerialInterface(port="/dev/ttyACM2", baudrate=115200, mock=False, auto_connect=True)
+interface = SerialInterface(port="/dev/ttyACM0", baudrate=115200, mock=False, auto_connect=True)
 controller = RuiyanHandController(communication_interface=interface,motors_id=[1,2,3,4,5,6], instruction=RuiyanHandInstructionType.CTRL_MOTOR_POSITION_VELOCITY_CURRENT)
 node = RuiyanHandNode(left_hand=controller,right_hand=None)
 rclpy.spin(node)
